cfa4cbf8dd95ae3430e3221b3a1597cebc4b30d8
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-02-10 12:22:37 +0530

    [Shubham Rajput] Add. Filereader problem2 : readafilelinebylineusingfilereader

---
+++ src/main/java/problemstatements/filereader/readafilelinebylineusingfilereader/ReadAFileLineByLineUsingFileReader.java
@@ -0,0 +1,29 @@
+package problemstatements.filereader.readafilelinebylineusingfilereader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+//creating ReadAFileLineByLineUsingFileReader class
+public class ReadAFileLineByLineUsingFileReader {
+
+    //creating mathod to readfile
+    public static String readFile(String string) {
+        String string1 = "";
+        // Use a try-with-resources block to ensure the file is closed after reading
+        try (BufferedReader br = new BufferedReader(new FileReader(string))) {
+            String line;
+
+            // Read each line from the file and print it to the console
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+                string1+=line;
+            }
+        } catch (IOException e) {
+            // Print stack trace in case of an exception
+            e.printStackTrace();
+        }
+        return string1;
+    }
+}
