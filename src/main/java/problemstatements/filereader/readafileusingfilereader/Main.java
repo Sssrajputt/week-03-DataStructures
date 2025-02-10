53c19560f79c5851665791b1cf56bd5c8454c86a
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-02-10 12:40:13 +0530

    [Shubham Rajput] Add. Filereader problem1 : filereader/readafilelinebylineusingfilereader1

---
+++ src/main/java/problemstatements/filereader/readafileusingfilereader/Main.java
@@ -0,0 +1,17 @@
+package problemstatements.filereader.readafileusingfilereader;
+
+import java.util.Scanner;
+
+//creating main class
+public class Main {
+    //creating main method
+    public static void main(String args[]){
+        //creating scanner class object
+        Scanner sc = new Scanner(System.in);
+        //taking user inout of a text file
+        System.out.println("Enter a txt file you wants to read: ");
+        String string = sc.nextLine();
+        //calling method to readfile
+        ReadAFileLineByLineUsing.readFile(string);
+    }
+}
